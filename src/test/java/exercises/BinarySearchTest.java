14161299c51fd149eb945efe3ad101c384958227
Ricardo <ricardohsmello@gmail.com>
2023-05-12 22:29:08 -0300

    ajdusting

--- src/test/java/exercises/BinarySearchTest.java
+++ src/test/java/exercises/BinarySearchTest.java
@@ -27,7 +27,6 @@ public class BinarySearchTest {
 
     @Test
     public void shouldTestBinarySearch03() {
-
         BinarySearch binarySearch = new BinarySearch();
         int[] nums = {-1,0,3,5,9,12};
         int target = 13;
