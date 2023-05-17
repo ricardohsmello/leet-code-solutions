35d61d8186f1fdbcc9bb26a42b95318b9f6429a9
Ricardo <ricardohsmello@gmail.com>
2023-05-16 23:31:24 -0300

    adjusting

--- src/test/java/exercises/RunningSumTest.java
+++ src/test/java/exercises/RunningSumTest.java
@@ -4,7 +4,6 @@ import br.com.ricas.exercises.RunningSum;
 import org.junit.jupiter.api.Test;
 
 public class RunningSumTest {
-
     @Test
     public void runningSum() {
         int [] numbers = {3,1,2,10,1};
