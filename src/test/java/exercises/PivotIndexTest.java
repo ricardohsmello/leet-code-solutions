d1d54131b90e22ed8bb1e089229a7e2073549a0a
Ricardo <ricardohsmello@gmail.com>
2023-05-21 01:01:19 -0300

    adjusting

--- src/test/java/exercises/PivotIndexTest.java
+++ src/test/java/exercises/PivotIndexTest.java
@@ -4,7 +4,6 @@ import br.com.ricas.exercises.PivotIndex;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class PivotIndexTest {
 
     @Test
