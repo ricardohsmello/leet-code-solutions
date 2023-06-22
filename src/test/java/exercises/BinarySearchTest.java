b3f8d9543cd4e3af97c93a4495ca13e2e8854256
Ricardo <ricardohsmello@gmail.com>
2023-06-21 23:22:48 -0300

    adjusting

--- src/test/java/exercises/BinarySearchTest.java
+++ src/test/java/exercises/BinarySearchTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BinarySearchTest {
-
     @Test
     public void shouldTestBinarySearch() {
         BinarySearch binarySearch = new BinarySearch();
