71da5faf1f7042028770f68578e1a99ec4777968
Ricardo <ricardohsmello@gmail.com>
2023-06-22 00:47:42 -0300

    adjusting

--- src/test/java/exercises/LongestSubstringTest.java
+++ src/test/java/exercises/LongestSubstringTest.java
@@ -2,7 +2,6 @@ package exercises;
 import br.com.ricas.exercises.LongestSubstring;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class LongestSubstringTest {
 
     @Test
